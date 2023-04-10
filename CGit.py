6cb98562cfb874d333a480d1ff3ae75b080a7edd
carlos <cargome409@gmail.com>
2023-04-10 20:15:05 -0300

    actulizacion

---
+++ CGit.py
@@ -0,0 +1,9 @@
+git reset -hard codigo de id -> sirve para rataurar el archivo 
+git log --online -> sirve para ver todos los commit
+git add . -> sirve para add a todos los archivo
+git commit -am -> hace un commit y un add al mismo tiempo
+git commit -m "texto" --amend -> para modificar sin que abrir VIM
+git remote add origin urr -> para concectar el repositor remoto
+git push -u origin rama -> para subir los cambios al repositor remoto
+git pull -> trae los cambios realizados desde GitHub o respositor remoto al repositor local
+git tag [nombre-version] -m "comentario" -> sirve para poner una version al proyecto
